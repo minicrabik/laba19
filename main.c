634ca9e044016839678660b38dd104b11df79f83
mini crabik <agl.malcv@gmail.com>
2024-05-27 18:40:14 +0300

    add evaluate and test

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include "task/matrixTransposition.h"
 #include "task/convertFloat.h"
+#include "task/evaluateExpression.h"
 
 void test() {
     test_matrix_transpose();
     test_convert_float();
+    test_evaluate();
 }
 
 int main() {
