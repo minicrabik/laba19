8f852cfc49af13d8d111ed080cc915497eabbf6c
mini crabik <agl.malcv@gmail.com>
2024-05-27 19:22:58 +0300

    add filter word and test

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include "task/matrixTransposition.h"
 #include "task/convertFloat.h"
 #include "task/evaluateExpression.h"
+#include "task/filterWord.h"
 
 void test() {
     test_matrix_transpose();
     test_convert_float();
     test_evaluate();
+    test_filter_word();
 }
 
 int main() {
